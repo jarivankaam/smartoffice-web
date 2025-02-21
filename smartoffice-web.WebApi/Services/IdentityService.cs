8f1060b5d229f6c4c3fc467ca506bb0d27d11bf0
Az3rAp1 <jjj.vankaam@student.avans.nl>
2025-02-21 17:09:29 +0100

    test

---
+++ smartoffice-web.WebApi/Services/IdentityService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace smartoffice_web.WebApi.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IdentityService(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<string> GetCurrentUserIdAsync(ClaimsPrincipal user)
+        {
+            var identityUser = await _userManager.GetUserAsync(user);
+            return identityUser?.Id ?? throw new UnauthorizedAccessException("User not found");
+        }
+    }
+}
