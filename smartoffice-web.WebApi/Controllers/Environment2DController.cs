e71722d40c63bb86da0d4f597256e4f1510d60e3
Az3rAp1 <jjj.vankaam@student.avans.nl>
2025-02-23 12:42:49 +0100

    smallish changes

--- smartoffice-web.WebApi/Controllers/Environment2DController.cs
+++ smartoffice-web.WebApi/Controllers/Environment2DController.cs
@@ -47,7 +47,7 @@ namespace smartoffice_web.WebApi.Controllers
             return Ok(world);
         }
 
-        [HttpPost]
+        [HttpPost("create")]
         [Authorize]
         public async Task<IActionResult> Create([FromBody] Environment2D environment2D)
         {
